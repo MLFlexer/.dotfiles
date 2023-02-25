2538e408a8c52e248bc1833af1ce3704c54efea5
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-02-25 21:31:23 +0100

    added lsp-lines and keymap to change old/new diagnostics

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- function to toggle "normal" diagnostics or lsp-lines diagnostics.
+local function toggle_diagnostics()
+  local diagnostics_on = require("lsp_lines").toggle()
+  if diagnostics_on then
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
+  else
+    vim.diagnostic.config({
+      virtual_text = { spacing = 4, prefix = "●" },
+    })
+  end
+end
+
+vim.keymap.set("n", "<Leader>ui", toggle_diagnostics, { desc = "Toggle [i]nline diagnostic type" })
---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  "neovim/nvim-lspconfig",
+  --turn of the default inline diagnostic to remove redundency from lsp-lines.
+  opts = {
+    diagnostics = {
+      virtual_text = false,
+    },
+  },
+}
