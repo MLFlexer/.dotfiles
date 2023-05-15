30ac5ade8b59147f6179fdd5095bb350309e22c5
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-05-15 20:22:43 +0200

    add symlink script

---
+++ symlink_config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -d "$HOME/old_dotfiles" ]; then
+	echo "removing $HOME/old_dotfiles/"
+	rm -rfd "$HOME/old_dotfiles"
+fi
+
+mkdir $HOME/old_dotfiles
+back_up_dir="$HOME/old_dotfiles"
+
+# symlink dotfiles:
+declare -a dot_arr
+dot_arr=(".gitconfig" ".zshenv")
+
+for file in ${dot_arr[@]}; do
+	if [ -f "$HOME/$file" ]; then
+		echo "moving $file to $HOME/old_dotfiles/"
+		mv $HOME/$file $back_up_dir/$file
+	fi
+	ln -s $PWD/$file $HOME/$file
+done
+
+# symlink .config-directories:
+
+mkdir $back_up_dir/.config
+
+declare -a conf_arr
+conf_arr=("alacritty" "nvim" "zsh" "nix" "tmux")
+
+for dir in ${conf_arr[@]}; do
+	if [ -d "$HOME/.config/$dir/" ]; then
+		echo "moving $dir to $HOME/old_dotfiles/.config/"
+		mv $HOME/.config/$dir $back_up_dir/.config/$dir
+	fi
+	ln -s $PWD/$dir $HOME/.config/$dir
+done
