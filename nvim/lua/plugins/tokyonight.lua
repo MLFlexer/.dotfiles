0dd704a94537116c740da80792ecca5b0b1ef7b4
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-05-25 21:05:24 +0200

    changed colorscheme

---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  { "folke/tokyonight.nvim" },
+
+  -- Configure LazyVim to load onedarker
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
+  },
+}
