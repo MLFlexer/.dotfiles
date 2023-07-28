b62ae2ebd008b0d5c48318185a6a531bc8ad5fdc
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-07-28 17:36:42 +0200

    add markdown keymap to display in browser

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+-- markdown preview keybind
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("markdown_preview"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.keymap.set("n", "<leader>o", "<Plug>MarkdownPreview", { desc = "Markdown preview", buffer = 0 })
+  end,
+})
