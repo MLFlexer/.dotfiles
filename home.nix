0af8df386cd82ba9861079df4a34e108d10ddd37
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-08-04 12:24:15 +0200

    moved home manager to root

--- home.nix
+++ home.nix
@@ -2,7 +2,11 @@
   config,
   pkgs,
   ...
-}: {
+}: 
+let
+  # path to dotfiles directory
+  dotdir = config.lib.file.mkOutOfStoreSymlink "/home/mlflexer/repos/.dotfiles";
+in {
   home = {
     stateVersion = "23.05";
     homeDirectory = "/home/mlflexer";
@@ -56,11 +60,55 @@
     ];
   };
 
+  # Symlink files
   home.file = { 
+    "alacritty" = {
+      enable = false;
+      source = "${dotdir}/alacritty";
+      recursive = true;
+      target = ".config/alacritty";
+    };
     "bat" = {
-      source = ./bat;
+      enable = true;
+      source = "${dotdir}/bat";
       recursive = true;
       target = ".config/bat";
+    };
+    "nvim" = {
+      enable = true;
+      source = "${dotdir}/nvim";
+      recursive = true;
+      target = ".config/nvim";
+    };
+    "wezterm" = {
+      enable = true;
+      source = "${dotdir}/wezterm";
+      recursive = true;
+      target = ".config/wezterm";
+    };
+    ".gitconfig" = {
+      enable = true;
+      source = "${dotdir}/.gitconfig";
+      recursive = false;
+      target = ".gitconfig";
+    };
+    ".zshenv" = {
+      enable = true;
+      source = "${dotdir}/.zshenv";
+      recursive = false;
+      target = ".zshenv";
+    };
+    "zsh" = {
+      enable = true;
+      source = "${dotdir}/zsh";
+      recursive = true;
+      target = ".config/zsh";
+    };
+    "tmux" = {
+      enable = true;
+      source = "${dotdir}/tmux";
+      recursive = true;
+      target = ".config/tmux";
     };
   };
 
