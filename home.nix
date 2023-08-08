d15b931bbbe2cce96fd75cf90da773545e52e8b0
Malthe Larsen <75012728+MLFlexer@users.noreply.github.com>
2023-08-08 17:47:49 +0200

    moved symlinking dotfiles into separate flake (#5)

--- home.nix
+++ home.nix
@@ -1,19 +1,13 @@
+{ config
+, pkgs
+, ...
+}:
 {
-  config,
-  pkgs,
-  ...
-}: 
-let
-  # path to dotfiles directory
-  dotdir = config.lib.file.mkOutOfStoreSymlink "/home/mlflexer/repos/.dotfiles";
-in {
-  fonts.fontconfig.enable = true;
   home = {
     stateVersion = "23.05";
     homeDirectory = "/home/mlflexer";
     username = "mlflexer";
     packages = with pkgs; [
-      (nerdfonts.override {fonts = [ "Meslo" ]; })
       bat
       broot
       cabal-install # Haskell
@@ -61,58 +55,6 @@ in {
       zsh-powerlevel10k
       zsh-syntax-highlighting
     ];
-  };
-
-  # Symlink files
-  home.file = { 
-    "alacritty" = {
-      enable = false;
-      source = "${dotdir}/alacritty";
-      recursive = true;
-      target = ".config/alacritty";
-    };
-    "bat" = {
-      enable = true;
-      source = "${dotdir}/bat";
-      recursive = true;
-      target = ".config/bat";
-    };
-    "nvim" = {
-      enable = true;
-      source = "${dotdir}/nvim";
-      recursive = true;
-      target = ".config/nvim";
-    };
-    "wezterm" = {
-      enable = true;
-      source = "${dotdir}/wezterm";
-      recursive = true;
-      target = ".config/wezterm";
-    };
-    ".gitconfig" = {
-      enable = true;
-      source = "${dotdir}/.gitconfig";
-      recursive = false;
-      target = ".gitconfig";
-    };
-    ".zshenv" = {
-      enable = true;
-      source = "${dotdir}/.zshenv";
-      recursive = false;
-      target = ".zshenv";
-    };
-    "zsh" = {
-      enable = true;
-      source = "${dotdir}/zsh";
-      recursive = true;
-      target = ".config/zsh";
-    };
-    "tmux" = {
-      enable = true;
-      source = "${dotdir}/tmux";
-      recursive = true;
-      target = ".config/tmux";
-    };
   };
 
   nixpkgs.config = {
