7bb5f2d85df4d5d31f808ec45a5f6314421cdb2b
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-08-10 13:01:24 +0200

    add gpaste

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -131,9 +131,11 @@
     vlc
     wget
     zip
+    gnome.gpaste # clipboard
     gnomeExtensions.gsconnect
   ];
 
+  programs.gpaste.enable = true;
   networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; }]; # Open ports for GSConnect
   networking.firewall.allowedUDPPortRanges = [{ from = 1714; to = 1764; }]; # Open ports for GSConnect
 
