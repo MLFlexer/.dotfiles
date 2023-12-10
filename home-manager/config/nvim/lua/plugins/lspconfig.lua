eae57255b0031509a5c8c83966ea26a7611756fd
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-12-10 15:43:06 +0100

    update lspconfig

--- home-manager/config/nvim/lua/plugins/lspconfig.lua
+++ home-manager/config/nvim/lua/plugins/lspconfig.lua
@@ -1,21 +1,40 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = { ---@type lspconfig.options
-      servers = {
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              lens = {
-                enable = true,
-              },
-              checkOnSave = {
-                command = "clippy",
-              },
+
+  "neovim/nvim-lspconfig",
+  opts = { ---@type lspconfig.options
+    servers = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            lens = {
+              enable = true,
+            },
+            checkOnSave = {
+              command = "clippy",
             },
           },
         },
       },
+      erlangls = {
+        mason = false,
+      },
     },
+    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+    setup = {
+      -- example to setup with typescript.nvim
+      -- tsserver = function(_, opts)
+      --   require("typescript").setup({ server = opts })
+      --   return true
+      -- end,
+      -- Specify * to use this function as a fallback for any server
+      -- ["*"] = function(server, opts) end,},
+    },
+    -- format = {
+    --   formatting_options = {
+    --     filter = function(client)
+    --       return client.name ~= "erlangls"
+    --     end,
+    --   },
+    -- },
   },
 }
