4b7ed8d84418c9caeee4f8d3cc00b0986a51260c
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-01-28 14:08:14 +0100

    update neovim plugins

--- home-manager/config/nvim/lua/plugins/vim-maximizer.lua
+++ home-manager/config/nvim/lua/plugins/vim-maximizer.lua
@@ -1,7 +1,27 @@
--- ability to toggle maximize window
 return {
-  "szw/vim-maximizer",
+  dir = "~/repos/nvim_plugs/maximizer.nvim/",
+  lazy = true,
+
+  init = function()
+    require("maximizer.tabs").setup()
+  end,
+
   keys = {
-    { "<leader>m", ":MaximizerToggle<CR>", desc = "[M]aximize toggle", mode = "n", silent = true },
+    -- { "<leader>m", ':lua require("maximizer").toggle()<CR>', desc = "[M]aximize toggle", mode = "n", silent = true },
+    -- {
+    --   "<leader>m",
+    --   ':lua require("maximizer.windows").toggle()<CR>',
+    --   -- require("maximizer").maximize_with_tab.toggle,
+    --   desc = "[M]aximize toggle",
+    --   mode = "n",
+    --   silent = true,
+    -- },
+    {
+      "<leader>M",
+      ':lua require("maximizer").save_view()<CR>',
+      desc = "[M]aximize toggle",
+      mode = "n",
+      silent = true,
+    },
   },
 }
