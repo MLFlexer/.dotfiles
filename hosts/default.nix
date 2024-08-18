99145a95f944f55464db5701853ab96885df3f78
MLFlexer <malthemlarsen@gmail.com>
2024-08-18 12:12:29 +0200

    initial working wsl

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,5 +3,7 @@
   laptop = import ./laptop { inherit inputs; };
   # Raspberry Pi 5 profile
   rpi5 = import ./rpi5 { inherit inputs; };
+	# Wsl
+  	wsl = import ./wsl { inherit inputs; };
   # other profiles ...
 }
---
+++ hosts/wsl/default.nix
@@ -0,0 +1,38 @@
+{ inputs, ... }:
+let
+  system = "x86_64-linux";
+  user = "mlflexer";
+
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+
+  unstable = import inputs.nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
+
+  lib = inputs.nixpkgs.lib;
+
+in lib.nixosSystem {
+  inherit system;
+  specialArgs = {
+    inherit pkgs unstable user inputs;
+    host = { hostName = "wsl"; };
+  };
+  modules = [ ./configuration.nix 
+	inputs.nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "24.05";
+            wsl.enable = true;
+          } 
+
+{
+	nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+}
+];
+}
