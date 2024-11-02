d12157d366081c90004b3c9aba40e45f7ba1eb46
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-11-02 16:20:33 +0100

    update vim

--- home-manager/config/nvim/lua/plugins/lspconfig.lua
+++ home-manager/config/nvim/lua/plugins/lspconfig.lua
@@ -1,38 +1,15 @@
 return {
-
   "neovim/nvim-lspconfig",
-  opts = { ---@type lspconfig.options
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
     servers = {
-      nixd = {},
-      rust_analyzer = {
-        settings = {
-          ["rust-analyzer"] = {
-            lens = {
-              enable = true,
-            },
-            checkOnSave = {
-              command = "clippy",
-            },
-          },
-        },
-      },
+      futhark_lsp = { mason = false },
     },
-    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
     setup = {
-      -- example to setup with typescript.nvim
-      -- tsserver = function(_, opts)
-      --   require("typescript").setup({ server = opts })
-      --   return true
-      -- end,
-      -- Specify * to use this function as a fallback for any server
-      -- ["*"] = function(server, opts) end,},
+      futhark_lsp = function()
+        require("lazyvim.util").lsp.on_attach(function(client) end)
+      end,
     },
-    -- format = {
-    --   formatting_options = {
-    --     filter = function(client)
-    --       return client.name ~= "erlangls"
-    --     end,
-    --   },
-    -- },
   },
 }
