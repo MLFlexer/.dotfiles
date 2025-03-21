c791928ad00ac26adef694b4d83723eb7038a5fe
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-03-21 19:39:02 +0100

    modualized home-manager

--- home-manager/modules/configs/bat.nix
+++ home-manager/modules/configs/bat.nix
@@ -1,19 +1,23 @@
-{ pkgs, ... }: {
-  programs.bat = {
-    enable = true;
-    config = {
-      pager = "less -FR";
-      theme = "tokoyonight_moon";
-    };
-    themes = {
-      tokoyonight_moon = {
-        src = pkgs.fetchFromGitHub {
-          owner = "folke";
-          repo = "tokyonight.nvim";
-          rev = "610179f7f12db3d08540b6cc61434db2eaecbcff";
-          sha256 = "sha256-mzCdcf7FINhhVLUIPv/eLohm4qMG9ndRJ5H4sFU2vO0=";
+{ pkgs, lib, config, ... }: {
+  options = { bat.enable = lib.mkEnableOption "Enables Bat config"; };
+
+  config = lib.mkIf config.bat.enable {
+    programs.bat = {
+      enable = true;
+      config = {
+        pager = "less -FR";
+        theme = "tokoyonight_moon";
+      };
+      themes = {
+        tokoyonight_moon = {
+          src = pkgs.fetchFromGitHub {
+            owner = "folke";
+            repo = "tokyonight.nvim";
+            rev = "610179f7f12db3d08540b6cc61434db2eaecbcff";
+            sha256 = "sha256-mzCdcf7FINhhVLUIPv/eLohm4qMG9ndRJ5H4sFU2vO0=";
+          };
+          file = "extras/sublime/tokyonight_moon.tmTheme";
         };
-        file = "extras/sublime/tokyonight_moon.tmTheme";
       };
     };
   };
--- home-manager/modules/configs/helix.nix
+++ home-manager/modules/configs/helix.nix
@@ -1,17 +1,18 @@
-{ config, config_dir, pkgs, system, inputs, ... }:
-let
-  # path to config directory
-  config_sym_dir = config.lib.file.mkOutOfStoreSymlink "${config_dir}";
+{ config, lib, system, inputs, ... }:
+let config_sym_dir = config.lib.file.mkOutOfStoreSymlink config.config_dir;
 in {
-  home.packages = with pkgs; [ inputs.helix-editor.packages.${system}.default ];
+  options = { helix.enable = lib.mkEnableOption "Enables Helix"; };
 
-  # Symlink files
-  home.file = {
-    "helix" = {
-      enable = true;
-      source = "${config_sym_dir}/helix";
-      recursive = true;
-      target = ".config/helix";
+  config = lib.mkIf config.helix.enable {
+    home.packages = [ inputs.helix-editor.packages.${system}.default ];
+
+    home.file = {
+      "helix" = {
+        enable = true;
+        source = "${config_sym_dir}/helix";
+        recursive = true;
+        target = ".config/helix";
+      };
     };
   };
 }
