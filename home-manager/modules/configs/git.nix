f1bcb8832964d328256a040e44b358062999f489
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-03-30 13:19:51 +0200

    update git config

--- home-manager/modules/configs/git.nix
+++ home-manager/modules/configs/git.nix
@@ -14,6 +14,53 @@
       };
       userEmail = "75012728+MLFlexer@users.noreply.github.com";
       userName = "MLFlexer";
+      # follows: https://blog.gitbutler.com/how-git-core-devs-configure-git/
+      extraConfig = {
+        diff = {
+          algorithm = "histogram";
+          colorMoved = true;
+          renames = true;
+          mnemonicPrefix = true;
+        };
+
+        init.defaultBranch = "main";
+
+        tag.sort = "version:refname";
+        branch.sort = "-committerdate";
+        column.ui = "auto";
+
+        push = {
+          autoSetupRemote = true;
+          followTags = true;
+        };
+
+        fetch = {
+          # prune = false;
+          # pruneTags = false;
+          all = true;
+        };
+        # pull.rebase true
+
+        help.autocorrect = "prompt";
+        commit.verbose = true;
+        merge.conflictstyle = "zdiff3";
+
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
+
+      };
+    };
+
+    # TODO: checkout the Slightly nicer rebase section of the article:  https://blog.gitbutler.com/how-git-core-devs-configure-git/
+    home.file = {
+      "ignore" = {
+        enable = true;
+        text = ".direnv";
+        recursive = false;
+        target = ".config/git/ignore";
+      };
     };
 
     programs.lazygit = {
