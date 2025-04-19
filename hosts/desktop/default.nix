718e2a060a1a803920997837f4e3ccf24afda6ca
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-04-19 23:16:21 +0200

    updated homemanger to use user variable

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -21,14 +21,18 @@ in lib.nixosSystem {
     inherit pkgs unstable user inputs;
     host = { hostName = "desktop"; };
   };
-  modules = [ ./configuration.nix ./hardware-configuration.nix
+  modules = [
+    ./configuration.nix
+    ./hardware-configuration.nix
     inputs.home-manager.nixosModules.home-manager
     {
-      home-manager.extraSpecialArgs = { inherit inputs system unstable; };
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.users.mlflexer = ./home.nix;
+      home-manager = {
+        extraSpecialArgs = { inherit inputs system unstable; };
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.${user} = ./home.nix;
+      };
     }
-     ];
+  ];
 }
 
