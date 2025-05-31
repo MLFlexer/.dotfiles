4034aaef6123a4657574284a6196d300632927f6
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-05-31 17:09:07 +0200

    update

--- hosts/rpi5/default.nix
+++ hosts/rpi5/default.nix
@@ -46,9 +46,31 @@ in lib.nixosSystem {
       };
 
       boot.initrd.kernelModules = [ "nvme" ];
+
     }
+    ({ config, pkgs, lib, ... }:
+      let kernelBundle = pkgs.linuxAndFirmware.v6_6_74;
+      in {
+        boot = {
+          loader.raspberryPi.firmwarePackage = kernelBundle.raspberrypifw;
+          kernelPackages = kernelBundle.linuxPackages_rpi5;
+        };
+
+        nixpkgs.overlays = lib.mkAfter [
+          (self: super: {
+            # This is used in (modulesPath + "/hardware/all-firmware.nix") when at least 
+            # enableRedistributableFirmware is enabled
+            # I know no easier way to override this package
+            inherit (kernelBundle) raspberrypiWirelessFirmware;
+            # Some derivations want to use it as an input,
+            # e.g. raspberrypi-dtbs, omxplayer, sd-image-* modules
+            inherit (kernelBundle) raspberrypifw;
+          })
+        ];
+      })
     inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.base
     ./configuration.nix
+
     # ../services
     # {
     #   home-assistant.enable = true;
