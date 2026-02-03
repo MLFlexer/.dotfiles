0b022ef783f4abc460180fa42c19f2d3446b7cfe
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2026-02-03 18:56:42 +0100

    update laptop config for 25.11

--- home-manager/laptop.nix
+++ home-manager/laptop.nix
@@ -3,18 +3,30 @@ let
   system = "x86_64-linux";
   pkgs = import inputs.nixpkgs {
     system = "${system}";
-    config = { allowUnfree = true; };
+    config = {
+      allowUnfree = true;
+    };
   };
   pkgs_unstable = import inputs.nixpkgs-unstable {
     system = "${system}";
-    config = { allowUnfree = true; };
+    config = {
+      allowUnfree = true;
+    };
   };
   user = "mlflexer";
   config_dir = "/home/${user}/repos/.dotfiles/home-manager/config";
 
-in inputs.home-manager.lib.homeManagerConfiguration {
+in
+inputs.home-manager.lib.homeManagerConfiguration {
   inherit pkgs;
-  extraSpecialArgs = { inherit config_dir pkgs_unstable inputs system; };
+  extraSpecialArgs = {
+    inherit
+      config_dir
+      pkgs_unstable
+      inputs
+      system
+      ;
+  };
   modules = [
     inputs.nix-index-database.hmModules.nix-index # comma integration
     ./modules
@@ -36,7 +48,7 @@ in inputs.home-manager.lib.homeManagerConfiguration {
       home = {
         username = "${user}";
         homeDirectory = "/home/${user}";
-        stateVersion = "25.05";
+        stateVersion = "25.11";
       };
 
       programs.home-manager.enable = true;
@@ -57,4 +69,3 @@ in inputs.home-manager.lib.homeManagerConfiguration {
     }
   ];
 }
-
